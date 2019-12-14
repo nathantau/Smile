c64bd837618909c5087b5474e7dbfaf20fda7e4d
nathan <nathan.au@kooltra.com>
2019-12-14 17:21:51 -0500

    Implemented true streaming

--- streamer.py
+++ streamer.py
@@ -3,7 +3,8 @@ import numpy as np
 from prediction import get_trained_model
 from prediction import predict_image
 from training import convert_to_3_channels, scale_coordinates
-from drawer import zip_to_coordinate_pairs
+from drawer import zip_to_coordinate_pairs, draw
+import matplotlib.pyplot as plt
 
 class Streamer():
 
@@ -12,18 +13,12 @@ class Streamer():
         self.model = get_trained_model('modelMKIII.h5')
 
     def stream(self):
-        # Do the prediction here, maybe import drawing methods
+
         stream = cv2.VideoCapture(0)
 
         while True:
 
             ret, frame = stream.read()
-
-            # frame is a numpy array, so we should do smth special w it
-
-            # frame = np.transpose(frame, (2, 0, 1))
-
-            # Mess with the frame for processing
 
             width = frame.shape[0]
             height = frame.shape[1]
@@ -46,20 +41,27 @@ class Streamer():
             output = np.array(output)
 
             zipped_coordinates = zip_to_coordinate_pairs(output)
-            print(zipped_coordinates)
+            print('zipped', zipped_coordinates)
 
-            frame_to_predict = cv2.cvtColor(frame_to_predict, cv2.COLOR_RGB2GRAY)
+            # In case I want to use the drawing mechanism
+
+            # output = scale_coordinates(output, 1.0/96.0)
+            # draw(frame_to_predict, output)
+
+            frame_to_predict = np.transpose(frame_to_predict, (2, 0, 1))[0]
 
             for coordinate_pair in zipped_coordinates:
                 x = int(coordinate_pair[0])
                 y = int(coordinate_pair[1])
 
-                for i in range(10):
-                    for j in range(10):
-                        frame_to_predict[x + i][y + j] = 1
+                for i in range(5):
+                    for j in range(5):
+                        frame_to_predict[y + i][x + j] = 0
 
-            # Reshape frame for display
-
+            frame_to_predict = frame_to_predict / 255
+            
+            cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('frame', 600,600)
             cv2.imshow('frame', frame_to_predict)
 
             if cv2.waitKey(33) == ord('q'):
